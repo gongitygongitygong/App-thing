f4740a27cc3424f2233d1d314512ff27b4f5c8eb
Zicheng <zc123playz@gmail.com>
2024-07-20 10:08:16 +0800

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Zicheng on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
