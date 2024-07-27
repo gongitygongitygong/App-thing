5ad6bf54be4a482e595d8a03558db2067d6fbb95
Zicheng <zc123playz@gmail.com>
2024-07-27 10:25:48 +0800

    idk what i did

--- project/ContentView.swift
+++ project/ContentView.swift
@@ -5,22 +5,25 @@
 //  Created by Zicheng on 20/7/24.
 //
 
-import SwiftUI
+import UIKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hll, world!")
-            Text("hi!")
-            Image(systemName:"square.and.arrow.up.badge.clock.fill")
-        }
-        .padding()
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myTextField = UITextField()
+        myTextField.placeholder = "Enter text here"
+        myTextField.borderStyle = .roundedRect
+        myTextField.textColor = .black
+        myTextField.font = UIFont.systemFont(ofSize: 16)
+
+        view.addSubview(myTextField)
+
+        myTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            myTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-}
-
-#Preview {
-    ContentView()
 }
