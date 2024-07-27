d17204dfda5ed4f04fe63971e7954a0e91b938cd
Zicheng <zc123playz@gmail.com>
2024-07-27 11:14:22 +0800

    cat

--- project/ContentView.swift
+++ project/ContentView.swift
@@ -5,25 +5,13 @@
 //  Created by Zicheng on 20/7/24.
 //
 
-import UIKit
+import SwiftUI
 
-class MyViewController: UIViewController {
+struct ContentView: View {
+    @State private var text: String = ""
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let myTextField = UITextField()
-        myTextField.placeholder = "Enter text here"
-        myTextField.borderStyle = .roundedRect
-        myTextField.textColor = .black
-        myTextField.font = UIFont.systemFont(ofSize: 16)
-
-        view.addSubview(myTextField)
-
-        myTextField.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            myTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            myTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+    var body: some View {
+        TextField("Enter your text here", text: $text)
+            .padding()
     }
 }
